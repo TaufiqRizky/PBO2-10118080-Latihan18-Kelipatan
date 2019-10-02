df3296bc13d32d4779241ad82b5cb098e855f720
Taufiq R <taufiq.it2@gmail.com>
2019-10-02 19:38:18 +0700

    first commit

---
+++ src/pbo2/pkg10118080/latihan18/kelipatan/PBO210118080Latihan18Kelipatan.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118080.latihan18.kelipatan;
+
+/**
+ *
+ * @author Trizky
+ */
+public class PBO210118080Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double i=3.5;
+        while(i<=35){
+            System.out.println(i);
+            i+=3.5;
+        }
+    }
+    
+}
